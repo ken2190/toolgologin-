699c98e8f288e7ad143dfcdf1ff4a7f6866ed3eb
danchoi4mua <89177451+danchoi4mua@users.noreply.github.com>
2022-08-25 07:54:44 +0700

    Update da_luong.py

--- da_luong.py
+++ da_luong.py
@@ -1,50 +1,75 @@
-import time
-import os
-from multiprocessing import Pool
-from sys import platform
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from gologin import GoLogin
-
-def scrap(profile):
-	gl = GoLogin({
-	        'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MmU5ZTU0MWRhN2Y3OGVjNTFhMzc0MjgiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2MmVhODNiNjAzNTQ2ZjFmYmViMWY3ZGYifQ.rL9AWo7jvHChxi6UIVdL5B1_capLaBYjULCEtJGzaoc',
-	        'profile_id': profile['profile_id'],
-	        'port': profile['port'],
-		})
-
-	if platform == "linux" or platform == "linux2":
-		chrome_driver_path = './chromedriver'
-	elif platform == "darwin":
-		chrome_driver_path = './mac/chromedriver'
-	elif platform == "win32":
-		chrome_driver_path = 'chromedriver.exe'
-
-	debugger_address = gl.start()
-	chrome_options = Options()
-	chrome_options.add_experimental_option("debuggerAddress", debugger_address)
-	driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
-	driver.get("http://www.python.org")
-	print('ready', profile['profile_id'], driver.title)
-	time.sleep(10)
-	print('closing', profile['profile_id'])
-	driver.close()
-	gl.stop()
-
-profiles = [
-	{'profile_id': 'profile_id_1', 'port': 3500},
-	{'profile_id': 'profile_id_2', 'port': 3501},
-	{'profile_id': 'profile_id_3', 'port': 3502},
-	]
+import  multiprocessing
+import taoprofile
 
 
-with Pool(3) as p:
-	p.map(scrap, profiles)
+def mofile():
+    f = open("acccanmo.txt", mode='r', encoding='utf-8')
+    tai_khoan = []
+    dia_chi=input("ban muon mo nuoc nao : ")
+    thu_muc_chua_profie = input("nhap thu muc chua profile (tên thư mục phải có định dạng như sau E:\\mu\\toolgmailusa\\profile ): ")
+    tokens = input("dien token cua ban : ")
+    for tk in f:
+        if tk == None :
+            continue
+        tk = tk.rstrip().split()
+        #tk[2] = "".join(tk[2:])
+        tk.append(dia_chi)
+        tk.append(tokens)
+        tk.append(dia_chi)
+        tai_khoan.append(tk)
+    f.close()
+    return tai_khoan
 
 
-if platform == "win32":
-	os.system('taskkill /im chrome.exe /f')
-	os.system('taskkill /im chromedriver.exe /f')
-else:
-	os.system('killall -9 chrome')
-	os.system('killall -9 chromedriver')
+
+def thu_tach_chuoi(tong_tai_khoan):
+    so_luong = int(input("so luong ban muon chay : " ))
+    bien=0
+    mang_chay = []
+    mang_cuoi=[]
+    for so_thu_tu in range(len(tong_tai_khoan)):
+        mang_chay.append(tong_tai_khoan[so_thu_tu])
+        bien+=1
+        if bien % so_luong ==0 or bien == len(tong_tai_khoan):
+            mang_cuoi.append(mang_chay)
+            mang_chay=[]
+    return mang_cuoi
+
+
+
+
+if __name__ == '__main__':
+
+    multiprocessing.freeze_support()
+    tong_tai_khoan = mofile()
+    mang_bo_vao_chay=thu_tach_chuoi(tong_tai_khoan)
+    #lay tai khoan trong ham mofile
+    luong=[]
+
+    for b in mang_bo_vao_chay:
+        for a in b :
+            chay = multiprocessing.Process(target=taoprofile.chay_tien_trinh, args=[a])
+            chay.start()
+            luong.append(chay)
+        for hetluong in luong:
+            hetluong.join()
+
+
+
+
+
+"""a=mofile()
+b=thu_tach_chuoi(a)
+for duc in b :
+    print(duc)"""
+
+
+
+
+
+
+
+
+
+
+
